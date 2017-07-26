157eb2b1bfd2ebc9e9a34221320eb2f01adb49d4
Hugo Schmitt <hugows@gmail.com>
2017-07-26 14:45:30 -0300

    Scrolling working better

--- screen.go
+++ screen.go
@@ -31,6 +31,7 @@ type Screen struct {
 	totalMatchCount int
 	selected        int // Selected match
 	stats           Stats
+	debug           map[string]int
 }
 
 func NewScreen() Screen {
@@ -43,6 +44,7 @@ func NewScreen() Screen {
 
 	screen.rootFolder = "."
 	screen.matches = make(map[string][]Match)
+	screen.debug = make(map[string]int)
 	screen.width, screen.height = termbox.Size()
 	return screen
 }
@@ -70,50 +72,80 @@ func (s *Screen) PrintCursor(x, y int) {
 	tbPrint(x, y, defaultCursorColor, defaultBgColor, " ")
 }
 
+func (s *Screen) Debug(key string, val int) {
+	s.debug[key] = val
+}
+
 func (s *Screen) Redraw() {
 	termbox.Clear(defaultFgColor, defaultBgColor)
 
 	// Top line is for user input / status messages.
 	line := 0
-	matchIdx := 0
 
 	//  To iterate in alphabetical order.
 	keys := []string{}
-	summCol := 0
+	maxFilePathWidth := 0
 
 	for k := range s.matches {
 		keys = append(keys, k)
-		if len(k) > summCol {
-			summCol = len(k)
+		if len(k) > maxFilePathWidth {
+			maxFilePathWidth = len(k)
 		}
 	}
 	sort.Strings(keys)
 
+	matchIdx := 0
 	// How many matches fit?
-	matchesCapacity := (s.height - 1) / 2
+
+	// Slow...?
+	matchesCapacity := 0
+Outer1:
+	for _, filepath := range keys {
+		line++
+		for range s.matches[filepath] {
+			line += 2
+			matchesCapacity++
+			if line >= s.height-2 {
+				break Outer1
+			}
+		}
+	}
+
 	matchesSkip := s.selected - matchesCapacity
+
+	s.Debug("line", line)
+	s.Debug("matchesCapacity", matchesCapacity)
+	s.Debug("matchesSkip", matchesSkip)
+	line = 0
 
 Outer:
 	for _, filepath := range keys {
 		f := s.matches[filepath]
 		col := 0
+		headerDrawn := false
 
 		//TODO: only print this if the section below is non empty
 
 		//summary := fmt.Sprintf("%sfilepath
-		tbPrint(col, line, defaultFilepathColor, defaultBgColor, filepath)
-		summ := fmt.Sprintf("%d matches", len(f))
-		tbPrint(summCol+2, line, defaultFilepathColor, defaultBgColor, summ)
-		line++
+		// Print this line if the "block" below is not empty.
 
 		for _, m := range f {
 			if matchIdx > matchesSkip {
+
+				if !headerDrawn {
+					tbPrint(col, line, defaultFilepathColor, defaultBgColor, filepath)
+					summ := fmt.Sprintf("%d matches", len(f))
+					tbPrint(maxFilePathWidth+1, line, defaultFilepathColor, defaultBgColor, summ)
+					line++
+					headerDrawn = true
+				}
+
 				line = m.Print(col, line, matchIdx == s.selected)
 			}
 			matchIdx++
 
 			// Dont draw off-screen
-			if line > s.height-2 {
+			if line > s.height-3 {
 				break Outer
 			}
 		}
@@ -126,17 +158,24 @@ Outer:
 	}
 
 	// Dump debug info
-	debugString := fmt.Sprintf("sel=%d", s.selected)
-	tbPrint(s.width-20, s.height-1, defaultFgColor, defaultBgColor, debugString)
+	// tbPrint(s.width-20, s.height-1, defaultFgColor, defaultBgColor, debugString)
 	//hiPrint(0, h-2, termbox.ColorGreen|termbox.AttrBold, "<	sel=%d>", s.selected)
 
 	// Status bar...
 	//tbPrint(0, s.h-1, defaultStatusColor, defaultBgColor, "QUERY >>> ")
-	x := hiPrint(0, s.height-1, defaultStatusColor, "Replace <%s> with <%s>? ", s.patternSearch, s.patternReplace)
+	//x := hiPrint(0, s.height-1, defaultStatusColor, "Replace <%s> with <%s>? ", s.patternSearch, s.patternReplace)
+
+	debugString := fmt.Sprintf("sel=%d ", s.selected)
+
+	for k, v := range s.debug {
+		debugString += fmt.Sprintf("%s=%d ", k, v)
+	}
+
+	tbPrint(0, s.height-1, defaultStatusColor, defaultBgColor, debugString)
 
 	//TODO: do it like this?
 	// s.UpdateStatus("Replace <%s> with <%s>? ", s.patternSearch, s.patternReplace)
-	s.PrintCursor(x, s.height-1)
+	// s.PrintCursor(x, s.height-1)
 
 	termbox.Flush()
 }
